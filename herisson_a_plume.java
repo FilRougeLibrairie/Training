edeadae73bc76bdec76e51425d6ce86423baee79
Guillaume Garvanèse <guillaume@garvanese.com>
2017-10-04 19:28:07 +0200

    ajout herisson_a_plume.java

---
+++ herisson_a_plume.java
@@ -0,0 +1,12 @@
+
+public class herisson_a_plume {
+
+    public static void main(String[] args) {
+
+        String name = "TOTO LE HERISSON A PLUMES";
+
+        System.out.println(name);
+
+    }
+
+}
