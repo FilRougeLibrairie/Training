ce942d4e52f11ab5dd7ba2073a0ed0cd2393d42d
cdi308@AFPA-PARIS-DI <cdi308@AFPA-PARIS-DI>
2017-10-09 09:37:12 +0200

    add Controller.java

---
+++ Controller.java
@@ -0,0 +1,98 @@
+package controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(name = "ControllerMain", urlPatterns = {"/ControllerMain"})
+public class ControllerMain extends HttpServlet {
+
+    private Cookie getCookie(Cookie[] cookies, String name) {
+        if (cookies != null) {
+            for (Cookie c : cookies) {
+                if (c.getName().equals(name)) {
+                    return c;
+                }
+            }
+        }
+        return null;
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+
+        HttpSession session = request.getSession();
+        String section = request.getParameter("section");
+        String page = "/WEB-INF/home.jsp";
+        /////// DO NOT MODIFY ABOVE THIS LINE ///////
+
+        /**
+         *
+         *
+         * HERE CODE FOR EACH SECTION
+         *
+         *
+         *
+         */
+        if ("menu-main".equals(section)) {
+            page = "/WEB-INF/includes/menu-main.jsp";
+        }
+
+        if ("catalog".equals(section)) {
+            page = "/WEB-INF/catalog.jsp";
+
+        }
+
+        /////// DO NOT MODIFY BELOW THIS LINE ///////
+        System.out.println("--------->>> page : " + page); // DEBUG : recursive calling if displayed twice
+        page = response.encodeURL(page);
+        getServletContext().getRequestDispatcher(page).include(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
